c53a576bba5391fdef75f6e0b0189193cc81af32
Nazyko <reypnazarovanazira@gmail.com>
2025-05-13 17:29:31 +0500

    Agro AI

--- src/components/our-servise/OurService.tsx
+++ src/components/our-servise/OurService.tsx
@@ -1,5 +1,8 @@
 import { Flex } from "@mantine/core"
 import "./OurService.css"
+import Card2 from "../../assets/Rectangle 6 (2).jpg"
+import Card3 from "../../assets/Rectangle 6.jpg"
+import Card1 from "../../assets/Rectangle 6 (1).jpg"
 
 
 export const OurService = () => {
@@ -11,14 +14,20 @@ export const OurService = () => {
                 Discover a comprehensive range of services tailored to meet your needs.
                 From strategic consultancy to hands-on implementation.
             </p>
-            <Flex className="service_cards">
+            <Flex className="service_cards" m={42} gap={44}>
                 <Flex className="service_card">
+                    <img className="service_card-img" src={Card1} alt="" />
+                    <div className="overlay"></div>
                     <h3 className="service_card-title">Farm Management Solutions</h3>
                 </Flex>
                 <Flex className="service_card">
+                    <img className="service_card-img" src={Card2} alt="" />
+                    <div className="overlay"></div>
                     <h3 className="service_card-title">Farm Management Solutions</h3>
                 </Flex>
                 <Flex className="service_card">
+                    <img className="service_card-img" src={Card3} alt="" />
+                    <div className="overlay"></div>
                     <h3 className="service_card-title">Farm Management Solutions</h3>
                 </Flex>
             </Flex>
